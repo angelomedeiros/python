4b64bd934d7fce0a6c4cf73615635fe231c67264
Angelo Medeiros <angelomedeiros3@gmail.com>
2017-08-12 15:38:29 -0300

    Estudos

---
+++ lista.py
@@ -0,0 +1,7 @@
+letra = [0,0,0,0,0]
+
+i = 0
+while i<5:
+    letra[i]=input("Letra: ")
+    i+=1
+
---
+++ variaveis.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+a = 2
+b = 3
+
+print("O resultado da soma 2 +3 é igual a ")
+print(a + b)
+
+'''
+==============   Python 2.x   ==================
+Imprime na mesma linha se tiver vírgula no final,
+caso não tenha ele pulará uma linha
+'''
+
+print "t",
+print "t",
+print "t",
+print "t",
+print "t"
+print "t"
+print "t"
+
+
+'''
+==============   Python 3.x   ==================
+'''
+'''
+print('t', end=' ')
+print('t', end='')
+print('t', end='')
+print('t', end='')
+print("t", end='')
+print('t')
+'''
