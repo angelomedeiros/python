bf83e6b3d4a18ced6ece73adaf0df0316e9b880b
Angelo Medeiros <angelomedeiros3@gmail.com>
2017-08-13 23:01:57 -0300

    - Quebra de linha em diferentes versoes

---
+++ print-exemplos.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+a = 2
+b = 3
+
+print("O resultado da soma 2 + 3 é igual a ",end="")
+#print("O resultado da soma 2 +3 é igual a ")
+print(a + b,end="!")
+print()
+
+'''
+==============   Python 2.x   ==================
+Imprime na mesma linha se tiver vírgula no final,
+caso não tenha ele pulará uma linha
+'''
+'''
+print "t",
+print "t",
+print "t",
+print "t",
+print "t"
+print "t"
+print "t"
+
+'''
+'''
+==============   Python 3.x   ==================
+Imprime pulando linha se o end='' tiver presente
+'''
+print('a', end="")
+print("b", end='  ')
+print("c", end='')
+print('d', end='  ')
+print("e", end='')
+print('f')
+print("Angelo")
