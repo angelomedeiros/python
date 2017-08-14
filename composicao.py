60c2fc92512a67da334c29627f6b9e871624fa8d
Angelo Medeiros <angelomedeiros3@gmail.com>
2017-08-13 22:53:12 -0300

    - O arquivo composicao.py possui exemplos de composicoes e fatiamento; - O arquivo print-exemplos.py explica como impedir a quabra de linha.

---
+++ composicao.py
@@ -0,0 +1,19 @@
+############################################
+#           Entrada de dados               #
+############################################
+
+name = input("Qual o seu nome? ")
+age = int(input("Qual a sua idade? "))
+height = float(input("Qual a sua altura? "))
+print("Meu nome é %s, tenho %d anos e tenho %.2fm de altura" %(name,age,height))
+
+############################################
+#               Fatiamento                 #
+############################################
+# Angelo #
+##########
+
+print(name[0:2]) #An
+print(name[:2]) #An
+print(name[2:]) #gelo
+print(name[-5:-1]) #ngel
