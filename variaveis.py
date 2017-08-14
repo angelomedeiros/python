6330d3aad349d07eecae152d1cfc3ca93acefacf
Angelo Medeiros <angelomedeiros3@gmail.com>
2017-08-13 21:16:14 -0300

    Editando arquivo variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -2,15 +2,17 @@
 a = 2
 b = 3
 
-print("O resultado da soma 2 +3 é igual a ")
-print(a + b)
+print("O resultado da soma 2 + 3 é igual a ",end="")
+#print("O resultado da soma 2 +3 é igual a ")
+print(a + b,end="!")
+print()
 
 '''
 ==============   Python 2.x   ==================
 Imprime na mesma linha se tiver vírgula no final,
 caso não tenha ele pulará uma linha
 '''
-
+'''
 print "t",
 print "t",
 print "t",
@@ -19,15 +21,15 @@ print "t"
 print "t"
 print "t"
 
-
+'''
 '''
 ==============   Python 3.x   ==================
+Imprime pulando linha se o end='' tiver presente
 '''
-'''
-print('t', end=' ')
-print('t', end='')
-print('t', end='')
-print('t', end='')
-print("t", end='')
-print('t')
-'''
+print('a', end="")
+print("b", end='  ')
+print("c", end='')
+print('d', end='  ')
+print("e", end='')
+print('f')
+print("Angelo")
